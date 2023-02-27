e49167564290be1a38c7bcb0360cb116a6f345f1
aurorevd <aurorevandamme@gmail.com>
2023-02-27 11:58:19 +0100

    fonts

--- src/Components/Home/LeftBar.jsx
+++ src/Components/Home/LeftBar.jsx
@@ -4,31 +4,30 @@ import RandomText from '../PostFunctions/RandomText'
 
 export default function LeftBar() {
   return (
-    
-        <div class="leftBar">
-            {/* bg-light */}
-            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 ">
-                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-secondary text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline">DashBoard</span>   
-                </a>
-                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start " id="menu">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link align-middle px-0">
-                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Friend Group</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Art group</span> </a>
-                        <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                            <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Collage group</span> </a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Doodle gang</span>  </a>
-                            </li>
-                        </ul>
-                    </li>
+    <div class="leftBar">
+        <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 ">
+            <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-secondary text-decoration-none">
+                <span class="fs-5 d-none d-sm-inline">DashBoard</span>   
+            </a>
+            <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start " id="menu">
+                <li class="nav-item">
+                    <a href="#" class="nav-link align-middle px-0">
+                        <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Friend Group</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                        <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Art group</span> 
+                    </a>
+                    <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                        <li class="w-100">
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Collage group</span> </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Doodle gang</span>  </a>
+                        </li>
+                    </ul>
+                </li>
                     <li>
                         <a href="#" class="nav-link px-0 align-middle">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Poetry group</span></a>
