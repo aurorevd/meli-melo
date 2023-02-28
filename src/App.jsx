dac460703cd299249d1535e501ff0788258f4a62
Ncarmend <113638375+Ncarmend@users.noreply.github.com>
2023-02-28 16:11:24 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Components/Home/HomePage"
 import AboutPage from "./Components/About/AboutPage"
 import ProfilePage from "./Components/UserProfile/ProfilePage";
-import {AuthProvider} from "./Components/Login/Auth"
+import { AuthProvider } from "./Components/Login/Auth"
 import LoginPage from "./Components/Login/LoginPage"
-import {RequireAuth} from './Components/Login/RequireAuth'
+import { RequireAuth } from './Components/Login/RequireAuth'
 import SignUp from './Components/Login/SignUp';
 import './Components/Home/Style.css'
 import PostPopUp from './Components/Home/PostPopUp';
 import ProfilePageLayout from './Components/UserProfile/ProfilePageLayout';
+import Groups from './Components/GroupUser/Groups'
 
 const history = createBrowserHistory();
 
@@ -20,12 +21,13 @@ const App = () => {
       <AuthProvider>
         <BrowserRouter history={history}>
           <Routes>
-          <Route path="/loginsignup" element={<SignUp/>}/>
-            <Route path="/" element={<HomePage />}/>
-            <Route path="/profile/" element={<RequireAuth><ProfilePage/></RequireAuth>}/>
-              <Route path="/loginsignup" element={<LoginPage />} /> 
-               <Route path = "/post" element={<PostPopUp/>}/>  
-               <Route path= "/profile" element={<ProfilePageLayout/>}/> 
+            <Route path="/loginsignup" element={<SignUp />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path='/groups' element={<Groups />} />
+            <Route path="/profile/" element={<RequireAuth><ProfilePage /></RequireAuth>} />
+            <Route path="/loginsignup" element={<LoginPage />} />
+            <Route path="/post" element={<PostPopUp />} />
+            <Route path="/profile" element={<ProfilePageLayout />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
--- src/Components/Home/NavBar.jsx
+++ src/Components/Home/NavBar.jsx
@@ -5,24 +5,24 @@ import '../Home/Style.css'
 import PostPopUp from "./PostPopUp";
 import axios from "axios"
 import Logo2 from "../../assets/meli.png"
-import { useState} from 'react';
+import { useState } from 'react';
 
 const NavBar = () => {
-    const [hover, setHover] = useState(false);
-    const handleHover = () => {
-      setHover(!hover);
-    };
-  
-    const logoStyle = {
-    
-      width: "250px",
-      height: "100px",
-      backgroundImage: `url(${hover ? Logo2 : Logo})`,
-      backgroundSize: "cover",
-      textDecoration: "none",
-      border: "none !important",
-  boxShadow: "none !important"
-    };
+  const [hover, setHover] = useState(false);
+  const handleHover = () => {
+    setHover(!hover);
+  };
+
+  const logoStyle = {
+
+    width: "250px",
+    height: "100px",
+    backgroundImage: `url(${hover ? Logo2 : Logo})`,
+    backgroundSize: "cover",
+    textDecoration: "none",
+    border: "none !important",
+    boxShadow: "none !important"
+  };
 
   const req = async () => {
     try {
@@ -34,12 +34,12 @@ const NavBar = () => {
       console.log("logout sucess")
     }
     catch (error) {
-    console.log(error);
+      console.log(error);
     }
   }
   return (
-  <div class="containerNavBar" >
-     <nav class="navbar  navbar-expand-lg navbar-light sticky container-xxl flex-wrap flex-lg-nowrap ">
+    <div class="containerNavBar" >
+      <nav class="navbar  navbar-expand-lg navbar-light sticky container-xxl flex-wrap flex-lg-nowrap ">
         <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon">
           </span>
@@ -48,43 +48,43 @@ const NavBar = () => {
         <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
           <ul class="navbar-nav  mt-2 mt-lg-0 justify-start">
             <li class="nav-item active">
-              <Link class="nav-link m-2"  to="/">
+              <Link class="nav-link m-2" to="/">
                 Home
               </Link>
             </li>
             <li class="nav-item">
-              <Link class="nav-link m-2"  to="/profile/:userId">
+              <Link class="nav-link m-2" to="/profile/:userId">
                 Profile
               </Link>
             </li>
             <li class="nav-item">
-              <Link class="nav-link width=auto m-2"  to="/groups">
+              <Link class="nav-link width=auto m-2" to="/groups">
                 Groups
               </Link>
             </li>
           </ul>
         </div>
-  
-        <Link href="#" style={{ ...logoStyle, border: 'none !important' }}  onMouseEnter={handleHover} onMouseLeave={handleHover} class="border-0 text-decoration-none object-fit-cover shadow-none" to="/" >
+
+        <Link href="#" style={{ ...logoStyle, border: 'none !important' }} onMouseEnter={handleHover} onMouseLeave={handleHover} class="border-0 text-decoration-none object-fit-cover shadow-none" to="/" >
         </Link>
-      
+
         <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo03">
           <div class="navbar-nav ms-auto mt-2 mt-lg-0">
-            <Link class="nav-link width=auto "  to="/loginsignup">
+            <Link class="nav-link width=auto " to="/loginsignup">
               <button type="button" class="btn  me-2 ">Log In
               </button>
             </Link>
-          
-            <Link class="nav-link width=auto "  to="/loginsignup">
+
+            <Link class="nav-link width=auto " to="/loginsignup">
               <button type="button" class="btn me-3">
                 Sign Up
               </button>
             </Link>
 
             <div class="nav-link width=auto">
-            <button onClick={req} type="button" class=" btn me-3">
-              Logout
-            </button>
+              <button onClick={req} type="button" class=" btn me-3">
+                Logout
+              </button>
             </div>
           </div>
         </div>
